d01c225f1af325c47eb2775a24b74084a8dc223d
TAURUS-ESSEN <reitarovea@gmail.com>
2025-09-28 18:45:25 +0200

    feat: add FutureJobs block

--- src/components/FutureJobs.jsx
+++ src/components/FutureJobs.jsx
@@ -1,7 +1,19 @@
 export default function FutureJobs({t}) {
+    const cards = t("futureJobs.cards");
     return (
         <div>
-            
+            <div><h2 className="h2">{t("futureJobs.title")} </h2></div>
+            <div className="flex gap-5">
+                    {cards.map((card,i) => (
+                        <div className={card.style} key={i}>
+                            <div className="text-base">{card.badge}</div>
+                            <div className="text-2xl">{card.title}</div>
+                            <div className="text-sm">{card.subtitle}</div>
+                            <div className="text-sm">{card.text}</div>
+                            <div className={card.buttonStyle}>{card.button}</div>
+                        </div>
+                    ))}
+            </div>
         </div>
     )
 }
