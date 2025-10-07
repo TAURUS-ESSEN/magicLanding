ca9bd4c745b24db6f66433f9a564cb58e4d749e9
TAURUS-ESSEN <reitarovea@gmail.com>
2025-10-07 13:49:19 +0200

    feat: implement mobile-first CSS adjustments2

--- src/Header.jsx
+++ src/Header.jsx
@@ -19,29 +19,30 @@ export default function Header({lang, switchLang}) {
                         <span className="close">☰</span>
                     </label>
 
-                <nav className="nav  md:flex">
-                    <ul> 
-                        <li><a href="/#programs">{t("menu.option1")}</a></li>
-                        <li><a href="/#payments">{t("menu.option2")}</a></li>
-                        <li><Link to="/about">{t("menu.option3")}</Link></li>
-                        <li><a href="/#contact">{t("menu.option4")}</a></li>
-                    </ul> 
-                    <div className="headerIconsBlock"> 
-                    <button className="headerIcon" onClick={switchLang}>
-                        {lang === "de" ? "EN" : "DE"}
-                    </button>
-                    <span className="headerIcon">
-                        <a href="https://wa.me/491701234567" target="_blank" aria-label={t("aria.whatsapp")}>
-                            <FontAwesomeIcon icon={faWhatsapp} size="2xl"  />
-                        </a>
-                    </span>
-                    <span className="headerIcon">
-                        <a href="https://share.google/kq74QHE7IJvYv9xQq" target="_blank" aria-label={t("aria.location")}>
-                            <FontAwesomeIcon icon={faLocationDot} size="2xl"/>
-                        </a>
-                    </span>
-                    </div>
-                </nav>
+                    <nav className="nav  md:flex">
+                        <ul> 
+                            <li><a href="/#programs">{t("menu.option1")}</a></li>
+                            <li><a href="/#payments">{t("menu.option2")}</a></li>
+                            <li><Link to="/about">{t("menu.option3")}</Link></li>
+                            <li><a href="/#contact">{t("menu.option4")}</a></li>
+                        
+                        <li className="headerIconsBlock"> 
+                            <button className="headerIcon" onClick={switchLang}>
+                                {lang === "de" ? "EN" : "DE"}
+                            </button>
+                            <button type="button" className="headerIcon md:text-xl">
+                                <a href="https://wa.me/491701234567" target="_blank" aria-label={t("aria.whatsapp")}>
+                                    <FontAwesomeIcon icon={faWhatsapp} size="2xl" />
+                                </a>
+                            </button>
+                            <button type="button" className="headerIcon md:text-xl">
+                                <a href="https://share.google/kq74QHE7IJvYv9xQq" target="_blank" aria-label={t("aria.location")}>
+                                    <FontAwesomeIcon icon={faLocationDot} size="2xl"/>
+                                </a>
+                            </button>
+                        </li>
+                        </ul> 
+                    </nav>
                 
  
             </header>
